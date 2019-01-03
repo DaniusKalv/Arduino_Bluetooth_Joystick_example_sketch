f2d352d2323ca6c2dcb0bc83c26bd28bffd3581f
Danius Kalvaitis <danius.kalvaitis@gmail.com>
2019-01-03 21:43:05 +0000

    Updated the code to support variable joystick range.
    Refactored variables, updated function names, added more defines.

--- Arduino_Bluetooth_Joystick_example_sketch.ino
+++ Arduino_Bluetooth_Joystick_example_sketch.ino
@@ -7,15 +7,18 @@
 #include <Servo.h> 
 
 //Hardware pin definitions
-#define A     2   //A pin of motor driver (L293D)
-#define B     3   //B pin of motor driver (L293D)
-#define EN    11  //EN pin of motor driver (L293D)
-#define SERVO 10  //Servo control pin 
+#define L293D_A       2   //A pin of motor driver (L293D)
+#define L293D_B       3   //B pin of motor driver (L293D)
+#define L293D_EN      11  //EN pin of motor driver (L293D)
+#define L293D_EN_MAX  255 //Maximum PWM value on L293D EN pin. It can be decreased if a maximum spped limit is desired.
+#define SERVO         10  //Servo control pin 
 
 //Default parameter definitions
-#define SERVO_LEFT_POSITION   0  //Value representing the left position of the servo (may require some tweeking)
-#define SERVO_MID_POSITION    30 //Value representing the mid position of the servo (may require some tweeking)
-#define SERVO_RIGHT_POSITION  60 //Value representing the right position of the servo (may require some tweeking)
+#define SERVO_LEFT_POSITION   0     //Value representing the left position of the servo (may require some tweeking)
+#define SERVO_MIDDLE_POSITION 30    //Value representing the mid position of the servo (may require some tweeking)
+#define SERVO_RIGHT_POSITION  60    //Value representing the right position of the servo (may require some tweeking)
+#define RANGE_FROM            0     //Set to the value selected in app settings (Settings->Joystick Value Range) 
+#define RANGE_TO              1024  //Set to the value selected in app settings (Settings->Joystick Value Range)
 
 Servo myservo; //Create a servo object 
 
@@ -24,10 +27,10 @@ int timeout_counter = 0; //Counter used for counting the time since the last rec
 //setup
 void setup() {
   Serial.begin(9600);     //Begin serial for interfacing with HC-05
-  setPwmFrequency(EN, 1); //Set PWM frequency of the EN pin to 32768Hz (above the frequency a human can hear, otherwise the motor is generating some noise). This function can be commented out or removed if this feature is not desired
+  setPwmFrequency(L293D_EN, 1); //Set PWM frequency of the EN pin to 32768Hz (above the frequency a human can hear, otherwise the motor is generating some noise). This function can be commented out or removed if this feature is not desired
   motorInitialize();      //Initialize the motor
   myservo.attach(SERVO);  //Attach a servo object
-  myservo.write(30);      //Put servo to the middle position
+  myservo.write(SERVO_MIDDLE_POSITION); //Put servo to the middle position
   delay(1000);            //Wait for the servo to go to the middle
 }
 
@@ -51,7 +54,7 @@ void loop() {
 void handleSerial(char c){
   switch(c){  //The first received character defines the packet type (speed, direction)
       case 'S':{  //'S' indicates that the speed packet was received
-        int y = Serial.parseInt() - 250; //Parse the speed (the received speed is between 0 and 500, 250 being the middle point, 0 representing full reverse and 500 representing full forward)
+        int y = Serial.parseInt(); //Parse the speed
         if(Serial.read() == 13){  //Check if the packet is complete, each packet ends with "\r\n" (13 and 10 in ANSII)
           if(Serial.read() == 10){
             motorDrive(y); //If the packet is valid, drive the motor
@@ -65,7 +68,7 @@ void handleSerial(char c){
         int x = Serial.parseInt(); //Read the direction (the received direction value is between 0 and 500)
         if(Serial.read() == 13){ //Check if the packet is complete, each packet ends with "\r\n" (13 and 10 in ANSII)
           if(Serial.read() == 10){
-            driveServo(x); //If the packet is valid, drive servo
+            servoDrive(x); //If the packet is valid, drive servo
           }
           else while(Serial.available()) Serial.read(); //Clear the Serial buffer if the packet is invalid
         }
@@ -75,24 +78,33 @@ void handleSerial(char c){
   }
 }  
   
-void driveServo(int x){
-  x = map(x, 0, 500, 0, 60); //Map the received data according to your servo parameters
+void servoDrive(int x){
+  x = map(x, RANGE_FROM, RANGE_TO, SERVO_LEFT_POSITION, SERVO_RIGHT_POSITION); //Map the received data according to your servo parameters
   myservo.write(x); //Drive servo
 }
 
-void motorDrive(int y){
-  if(y > 0) motorForward(y); //Positive value represents forward movement
-  else if(y < 0) motorBackward((-1) * y); //Negative value represents backward movement
-  else motorStop(); //If y equals 0 (Joystick is in the mid position), stop the motor  
+//Initialise pins for driving L293D IC  
+void motorInitialize(){
+  pinMode (L293D_A, OUTPUT);
+  pinMode (L293D_B, OUTPUT);
+  digitalWrite(L293D_A, 0);
+  digitalWrite(L293D_B, 0);
+  analogWrite(L293D_EN, 0);  
 }
 
-void motorInitialize(){
-  //Initialise pins for driving L293D IC  
-  pinMode (A, OUTPUT);
-  pinMode (B, OUTPUT);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  analogWrite(EN, 0);  
+void motorDrive(int y){
+  y = y - RANGE_TO/2; //Subtract the mid point value to determine if we are going forward or backward
+
+  if(y > 0){ //Positive value represents forward movement 
+    y = map(y, RANGE_FROM, RANGE_TO, 0, L293D_EN_MAX); //Map the speed value to L293D_EN_MAX limit
+    motorForward(y);    
+  } 
+  else if(y < 0){ //Negative value represents backward movement
+    y = (-1) * y; //We know that we are going backward, let's make y positive again
+    y = map(y, RANGE_FROM, RANGE_TO, 0, L293D_EN_MAX); //Map the speed value to L293D_EN_MAX limit
+    motorBackward(y);
+  } 
+  else motorStop(); //If y equals 0 (Joystick is in the mid position), stop the motor  
 }
 
 void motorForward(int velocity){
@@ -100,9 +112,9 @@ void motorForward(int velocity){
   if (velocity < 0) velocity = 0;
   else if (velocity > 255) velocity = 255;
 
-  analogWrite(EN, velocity); //Set speed
-  digitalWrite(A, 1); //Set direction
-  digitalWrite(B, 0);  
+  analogWrite(L293D_EN, velocity); //Set speed
+  digitalWrite(L293D_A, 1); //Set direction
+  digitalWrite(L293D_B, 0);  
 }
 
 void motorBackward(int velocity){
@@ -110,21 +122,21 @@ void motorBackward(int velocity){
   if (velocity < 0) velocity = 0;
   else if (velocity > 255) velocity = 255;
 
-  analogWrite(EN, velocity); //Set speed
-  digitalWrite(A, 0); /Set direction
-  digitalWrite(B, 1);    
+  analogWrite(L293D_EN, velocity); //Set speed
+  digitalWrite(L293D_A, 0); //Set direction
+  digitalWrite(L293D_B, 1);    
 }
 
 void motorStop(){
-  analogWrite(EN, 255); //Stop the motor by shorting its both connections to GND
-  digitalWrite(A, 1);
-  digitalWrite(B, 1);   
+  analogWrite(L293D_EN, 255); //Stop the motor by shorting its both connections to GND
+  digitalWrite(L293D_A, 1);
+  digitalWrite(L293D_B, 1);   
 }
 
 void motorOff(){
-  analogWrite(EN, 0); //Shut down the L293D
-  digitalWrite(A, 0);
-  digitalWrite(B, 0); 
+  analogWrite(L293D_EN, 0); //Shut down the L293D
+  digitalWrite(L293D_A, 0);
+  digitalWrite(L293D_B, 0); 
 }
 
 //This function does some register magic to set PWM to a custom frequency
